3577a7be4c584bd1e001968e8f1b758927c40b3d
Raj Patel <raj.patel@flynncompanies.com>
2022-02-01 19:05:00 -0500

    Implement Controller and interface for basic Order functions

--- PizzaDelivery/Controllers/OrderController.cs
+++ PizzaDelivery/Controllers/OrderController.cs
@@ -1,11 +1,55 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PizzaDelivery.Interfaces;
+using PizzaDelivery.Models.Entities;
 
 namespace PizzaDelivery.Controllers
 {
+    // All controllers should have authentication and authorization logic
+    // [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class OrderController : ControllerBase
     {
+        #region private fields
+
+        private readonly IOrderService _orderService;
+
+        #endregion
+        public OrderController(
+            IOrderService orderService
+        )
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet("{orderId}")]
+        public async Task<IActionResult> GetOrder(int orderId)
+        {
+            var order = await _orderService.GetOrder(orderId).ConfigureAwait(false);
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PlaceOrder(Order order)
+        {
+            var createdOrder = await _orderService.CreateOrder(order).ConfigureAwait(false);
+            return Ok(createdOrder);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrder(Order order)
+        {
+            var createdOrder = await _orderService.UpdateOrder(order).ConfigureAwait(false);
+            return Ok(createdOrder);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> CancelOrder(int orderId)
+        {
+            var createdOrder = await _orderService.CancelOrder(orderId).ConfigureAwait(false);
+            return Ok(createdOrder);
+        }
+
     }
 }
---
+++ PizzaDelivery/Interfaces/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using PizzaDelivery.Models.Entities;
+
+namespace PizzaDelivery.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrder(Order order);
+        Task<Order> UpdateOrder(Order order);
+        Task<Order> GetOrder(int orderId);
+        Task<Order> CancelOrder(int order);
+    }
+}
